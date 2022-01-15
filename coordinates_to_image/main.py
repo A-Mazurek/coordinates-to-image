97cbaf352746ec01203ba5f0ed5c71e31b7494bd
Andrzej Mazurek <andrzejmazurek@MacBook-Pro.local>
2022-01-15 19:01:54 +0100

    Add draft for basic check

--- coordinates_to_image/main.py
+++ coordinates_to_image/main.py
@@ -9,11 +9,15 @@ import click
 @click.option('--zoom', 'zoom_start', help="Map zoom in range 1 to 22")
 @click.option('--size', 'size', help="Enter size map in tuple e.g. (400, 400)")
 def entrypoint(coordinates, output_image, zoom_start, size):
+    coordinates = [51.513853791419734, -0.12571144384615038]
+    output_image = 'coordinates_map.png'
+    zoom_start = 17
+    size = (400, 400)
     m = folium.Map(location=coordinates, zoom_start=zoom_start, zoomControl=False)
-    m.save('tmp/coordinates_map.html')
+    m.save('html/coordinates_map.html')
 
-    hti = Html2Image(custom_flags=['--virtual-time-budget=10000', '--hide-scrollbars'])
-    hti.screenshot(html_file='tmp/coordinates_map.html', save_as=output_image, size=size)
+    hti = Html2Image(custom_flags=['--virtual-time-budget=10000', '--hide-scrollbars'], output_path='images')
+    hti.screenshot(html_file='html/coordinates_map.html', save_as=output_image, size=size)
 
 
 if __name__ == '__main__':
